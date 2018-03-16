a600b6a996c769b1f3b47cd7b78e858d2b84527c
Sravani663 <sravanims2015@gmail.com>
2018-03-15 21:29:36 -0700

    insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+def insertion_sort(array):
+    for index in range(1, len(array)):
+        while 0 < index and array[index] < array[index - 1]:
+            array[index], array[
+                index - 1] = array[index - 1], array[index]
+            index -= 1
+
+    return array
+    
+if __name__ == '__main__':
+    try:
+        raw_input        
+    user_input = raw_input('Enter numbers separated by a comma:\n').strip()
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(insertion_sort(unsorted))
